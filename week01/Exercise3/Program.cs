fea7fae16a71126b1ac1a79d88b6088e0abbb1a3
RaiFlores05 <rflores@byupathway.edu>
2026-07-04 13:08:10 -0500

    Finished Exercise #3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+       
+    //Console.Write("What is the magic number?");
+    //int magicNumber = int.Parse(Console.ReadLine());
+
+    Random randomGenerator = new Random();
+    int number = randomGenerator.Next(1, 11);
+
+    Console.WriteLine("Welcome to the Number Guessing Game!");
+    Console.WriteLine("Please guess a number between 1 and 11");
+    
+
+    
+    
+    int guess = 0;
+
+        // We could also use a do-while loop here...
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
+
     }
+    
 }

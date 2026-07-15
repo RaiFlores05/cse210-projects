3a7baf0f1376ea24c45a746694d8452e7c1af168
RaiFlores05 <rflores@byupathway.edu>
2026-07-15 09:09:06 -0500

    Finished W03 Learning Acitvity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction = new Fraction();
+        Fraction fraction1 = new Fraction(6);
+        Fraction fraction2 = new Fraction(6/7);
+
+        
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
+
+        fraction1.SetTop(5);
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        fraction2.SetBottom(4);
+        fraction2.SetTop(3);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        fraction2.SetTop(1);
+        fraction2.SetBottom(3);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
     }
 }
